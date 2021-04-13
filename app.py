2b2499ab915c12503338442c59ee49d96f1aaef6
Cole Koerner <colekoerner.ck@gmail.com>
2021-04-12 19:30:20 -0500

    updates 4/12

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, redirect, url_for
+from flask import Flask, render_template
 
 DEBUG = True
 
@@ -7,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/todolist')
+def todolist():
+    return render_template('todolist.html')
+
+# if __name__ == '__main__':
+#     app.run(host = '0.0.0.0', port = 5000, debug = True)
